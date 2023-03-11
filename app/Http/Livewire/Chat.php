e2839109db2bfc656164e67fa65b1e94a791b1c2
seham2023 <sehamali47888@gmail.com>
2023-03-11 23:36:40 +0200

    update

--- app/Http/Livewire/Chat.php
+++ app/Http/Livewire/Chat.php
@@ -2,17 +2,33 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use App\Models\Message;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Storage;
 
 class Chat extends Component
 {
+    public $receiver;
 
     public $messageText;
 
+    public function mount(User $receiver)
+    {
+        $this->receiver = $receiver;
+    }
+
+
     public function render()
     {
-        $messages = Message::with('user')->latest()->take(10)->get()->sortBy('id');
+        $messages = Message::where(function ($query) {
+            $query->where('sender_id', auth()->id())
+                ->where('receiver_id', $this->receiver->id);
+        })->orWhere(function ($query) {
+            $query->where('sender_id', $this->receiver->id)
+                ->where('receiver_id', auth()->id());
+        })->latest()->take(10)->get()->sortBy('id');
 
         return view('livewire.chat', compact('messages'));
     }
@@ -20,10 +36,14 @@ class Chat extends Component
     public function sendMessage()
     {
         Message::create([
-            'user_id' => auth()->user()->id,
+            'sender_id' => auth()->user()->id??null,
+            'receiver_id' => $this->receiver->id,
             'message' => $this->messageText,
         ]);
 
         $this->reset('messageText');
     }
+
+
+
 }
--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -5,6 +5,19 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <livewire:styles />
+    <style>.outgoing_msg {
+        margin-left: auto;
+    }
+
+    .incoming_msg {
+        margin-right: auto;
+    }
+
+    .msg_history {
+        overflow-y: auto;
+        padding: 10px;
+    }
+    </style>
 </head>
 <body>
     <header>
@@ -12,16 +25,15 @@
     </header>
 
     <main>
-        <div class="container mt-5">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col-md-8 offset-md-2">
                     <div class="card">
                         <div class="card-header">
                             Chat System
                         </div>
-                        <div class="card-body">
-                            <livewire:chat/>
-                        </div>
+                        <livewire:chat :receiver="$receiver"/>
+
                     </div>
                 </div>
             </div>
